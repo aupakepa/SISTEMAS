aae18bf1f3a5ccd2932cea0cce487c39c9c0623b
aupakepa <jmonforte@fontecabras.es>
2017-12-18 00:02:02 +0100

    eje703.sh

--- eje703.sh
+++ eje703.sh
@@ -6,13 +6,25 @@
 #listar hasta esta linea
 FICHERO=$1
 LINEAS=`cat $1 | wc -l`
-INICIO=1
-FIN=`expr $LINEAS - 1`
-if [ $3 -lt $2 ]
+
+if [ $2 -z ]
+then
+	INICIO=1
+else
+	INICIO=$2
+fi
+if [ $3 -z ]
+then
+	FIN=$LINEAS
+else
+	FIN=$3
+fi
+
+if [ $FIN -lt $INCIO ]
 then
 	echo error el parametro 3 es menor que el dos
 	exit 3
-elif [ $2 -gt $LINEAS -o $3 -gt $LINEAS ]
+elif [ $FIN -gt $LINEAS -o $INICIO -gt $LINEAS ]
 then
 	echo el inicio o el fin  es mayor que el numero de lineas totales
 	exit 3
@@ -24,8 +36,8 @@ elif [ ! -r $FICHERO ]
 then
 	echo el usuario no tiene permiso de lectura
 else
-	INICIO=`expr $2 - 1`
-	FIN=`expr $LINEAS - $3 + 1`
+	INICIO=`expr $INICIO - 1`
+	FIN=`expr $LINEAS - $FIN + 1`
 	cat $FICHERO | tail -n +$INICIO | head -n -$FIN 2>/dev/null >salida
 	FICHERO=./salida	
 	CONT=$INICIO
